4ad8053bfddb4838d265f8b4eea205e22921b9e6
nagashima8510 <tetsuya_nagashima@icloud.com>
2021-09-25 16:38:19 +0900

    first commit

---
+++ Constants.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#’PˆÊ•ÏŠ·ŒW”
+m2ft=3.28084
+ft2m=0.3048
+kt2mps=0.514444
+mps2kt=1.94384
+rad2deg=180/np.pi
+deg2rad=np.pi/180
+lb2kg=0.453592
+kg2lb=2.20462
+sec2min=1/60
+min2sec=60
+
+#‘å‹CŠÖ˜A’è”
+gamma=1.4
+mu=(gamma-1)/gamma
+R=287.05287 #[m2/(Ks2)]
+g0=9.80665 #[m/s2]
